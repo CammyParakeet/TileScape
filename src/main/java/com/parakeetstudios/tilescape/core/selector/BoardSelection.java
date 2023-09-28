59cfbc00d3bccbf7fb2f6ab75941466dd254efd8
SheeshSpace <cammy.sheesh@gmail.com>
2023-09-29 00:41:52 +1300

    foundation for a selection

---
+++ src/main/java/com/parakeetstudios/tilescape/core/selector/BoardSelection.java
@@ -0,0 +1,67 @@
+package com.parakeetstudios.tilescape.core.selector;
+
+import com.google.inject.assistedinject.Assisted;
+import com.google.inject.assistedinject.AssistedInject;
+import com.parakeetstudios.tilescape.data.TilescapeConfig;
+import com.parakeetstudios.tilescape.game.piece.Piece;
+import org.bukkit.Color;
+import org.bukkit.block.Block;
+import org.bukkit.entity.BlockDisplay;
+import org.jetbrains.annotations.NotNull;
+
+public class BoardSelection implements Selection {
+
+    private Block currentBlock;
+    private BlockDisplay selectionDisplay;
+    private Color color;
+    private Piece currentPiece;
+
+    private final TilescapeConfig cfg;
+
+    @AssistedInject
+    public BoardSelection(
+            @Assisted Block currentBlock,
+            @Assisted Color color,
+            @Assisted Piece piece,
+            @NotNull TilescapeConfig cfg)
+    {
+        this.cfg = cfg;
+        this.currentBlock = currentBlock;
+        this.color = color;
+        this.currentPiece = piece;
+        //this.selectionDisplay = some spawn util method
+    }
+
+
+    @Override
+    public Block getCurrentBlock() {
+        return currentBlock;
+    }
+
+    @Override
+    public BlockDisplay getDisplay() {
+        return selectionDisplay;
+    }
+
+    @Override
+    public Color getColor() {
+        return color;
+    }
+
+    @Override
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public Piece getSelectedPiece() {
+        return currentPiece;
+    }
+
+    @Override
+    public void remove() {
+        this.currentPiece = null;
+        this.selectionDisplay.remove();
+        this.currentBlock = null;
+    }
+}
--- src/main/java/com/parakeetstudios/tilescape/core/selector/Selection.java
+++ src/main/java/com/parakeetstudios/tilescape/core/selector/Selection.java
@@ -1,15 +1,21 @@
 package com.parakeetstudios.tilescape.core.selector;
 
+import com.parakeetstudios.tilescape.game.piece.Piece;
 import org.bukkit.Color;
+import org.bukkit.block.Block;
 import org.bukkit.entity.BlockDisplay;
 
 public interface Selection {
+
+    //TODO rework to use interaction?
+    Block getCurrentBlock();
 
     BlockDisplay getDisplay();
 
     Color getColor();
     void setColor(Color color);
 
-    
+    Piece getSelectedPiece();
 
+    void remove();
 }
